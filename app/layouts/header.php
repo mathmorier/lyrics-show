b245b55a109442357f66dfbf483de1dbd393f241
mathmorier <math.morier@gmail.com>
2022-05-17 19:54:30 -0600

    css en cours

--- app/layouts/header.php
+++ app/layouts/header.php
@@ -4,21 +4,24 @@
         <div class="line"></div>
         <div class="line"></div>
     </div>
-    <div id="slider-princ" class="slider">
+    <div id="slider-princ" class="slider show">
         <ul>
             <li><a href="/">home</a></li>
             <li><a href="/lyrics">lyrics</a></li>
             <li><a href="/os<?=$main['api_path'] ?? ""?>">OpenSong</a></li>
         </ul>
-        <div id="list-item"></div>
-        <div id="list-btn">
-            <button id="save-lyrics">S</button>
-            <button id="clear-lyrics">D</button>
-            <button id="share-lyrics">></button>
+        <div id="list">
+            <div id="list-btn">
+                <button id="save-lyrics"><i class="fa-solid fa-heart"></i></button>
+                <button id="clear-lyrics"><i class="fa-solid fa-trash"></i></button>
+                <button id="share-lyrics"><i class="fa-solid fa-share-nodes"></i></button>
+            </div>
+            <div id="list-link">
+                <input type="text" id="link">
+                <button id="copy-link"><i class="fa-solid fa-copy"></i></button>
+            </div>
+            <div id="list-item"></div>
         </div>
-        <div id="list-link">
-            <input type="text" id="link">
-            <button id="copy-link">C</button>
-        </div>
+ 
     </div>
 </nav>
