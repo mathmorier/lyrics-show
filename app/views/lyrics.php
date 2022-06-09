9bfa4f56a6e3e7810b045719d4f74aa88c06c5cb
mathmorier <math.morier@gmail.com>
2022-06-08 19:38:50 -0600

    Add banner full screen and save

--- app/views/lyrics.php
+++ app/views/lyrics.php
@@ -1,9 +1,13 @@
 <div class="search-genuis">
     <?=$main['src']['searchGenius'] ?? ''?>
 </div>
-<button id="like">
+<!-- <button id="like">
     <i class="fa-solid fa-heart"></i>
-</button>
+</button> -->
+<div class="banner">
+    <button id="full"><i class="fa-solid fa-expand"></i>Full Screen</button>
+    <button id="like"><i class="fa-solid fa-heart"></i>Save</button>
+</div>
 <div>
     <?=$song->embed_content ?? ''?>
 </div>
--- public/js/lyrics.js
+++ public/js/lyrics.js
@@ -1,4 +1,5 @@
 let btnLike = document.querySelector('#like')
+let btnFull= document.querySelector('#full')
 
 window.onload = function () {
     if (localStorage.getItem('id') == "") {
@@ -9,7 +10,7 @@ window.onload = function () {
             li.forEach(e => {
                 if (e.id == localStorage.getItem('id')) {
                     btnLike.children[0].classList.remove('fa-heart')
-                    btnLike.children[0].classList.add('fa-circle-check')
+                    btnLike.children[0].classList.add('fa-check')
                     btnLike.disabled = true;
                 }
             });
@@ -27,7 +28,7 @@ btnLike.addEventListener('click', function () {
         )
     if (localStorage.getItem('id')) {
         this.children[0].classList.remove('fa-heart')
-        this.children[0].classList.add('fa-circle-check')
+        this.children[0].classList.add('fa-check')
         this.disabled = true
         listItem = showList(listItem)
         btnOpenPrinc.classList.add('show');
@@ -35,3 +36,63 @@ btnLike.addEventListener('click', function () {
     }
 })
 
+btnFull.addEventListener('click', function () {
+    if (changeFullNoFull()) {
+        this.children[0].classList.remove('fa-compress')
+        this.children[0].classList.add('fa-expand')
+    }else{
+        this.children[0].classList.remove('fa-expand')
+        this.children[0].classList.add('fa-compress')
+    }
+    
+    // this.disabled = true
+})
+
+function changeFullNoFull() {
+
+    var isInFullScreen = (document.fullscreenElement && document.fullscreenElement !== null) ||
+    (document.webkitFullscreenElement && document.webkitFullscreenElement !== null) ||
+    (document.mozFullScreenElement && document.mozFullScreenElement !== null) ||
+    (document.msFullscreenElement && document.msFullscreenElement !== null);
+
+    var docElm = document.documentElement;
+    if (!isInFullScreen) {
+        if (docElm.requestFullscreen) {
+            docElm.requestFullscreen();
+        } else if (docElm.mozRequestFullScreen) {
+            docElm.mozRequestFullScreen();
+        } else if (docElm.webkitRequestFullScreen) {
+            docElm.webkitRequestFullScreen();
+        } else if (docElm.msRequestFullscreen) {
+            docElm.msRequestFullscreen();
+        }else{
+            alert('Use F11 for full screen')
+        }
+    } else {
+        if (document.exitFullscreen) {
+            document.exitFullscreen();
+        } else if (document.webkitExitFullscreen) {
+            document.webkitExitFullscreen();
+        } else if (document.mozCancelFullScreen) {
+            document.mozCancelFullScreen();
+        } else if (document.msExitFullscreen) {
+            document.msExitFullscreen();
+        }else{
+            alert('Use F11 for exit full screen')
+        }
+    }
+    return isInFullScreen;
+    // var player = document.documentElement
+
+    // if (player.requestFullscreen){
+    //     player.requestFullscreen();
+    // }
+    // else if (player.mozRequestFullScreen){
+    //     player.mozRequestFullScreen(); // Firefox
+    // }
+    // else if (player.webkitRequestFullscreen){
+    //     player.webkitRequestFullscreen(); // Chrome and Safari
+    // }else{
+    //     alert('Use F11 for full screen')
+    // }
+}
