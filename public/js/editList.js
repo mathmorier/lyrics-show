2f0255d7b03209157a9f15c8610cc6e2cb7cf292
mathmorier <math.morier@gmail.com>
2022-06-14 15:58:57 -0600

    Update editList.js

--- public/js/editList.js
+++ public/js/editList.js
@@ -1,75 +1,81 @@
 let listItemEdit = document.querySelector('.list-item-edit')
 
-listItemEdit = showEditList(listItemEdit)
+listItemEdit.innerHTML = ""
+let list = JSON.parse(localStorage.getItem('saveList'))
+
+// console.log(list);
+
+if (list!=null) {
+    list.forEach((e, key) => {
+        let img = document.createElement('img')
+        img.src = e.song_art_image_thumbnail_url
+        img.setAttribute("width",   35);
+    img.setAttribute("height",  35);
+
+    let up      = document.createElement('button')
+    let down    = document.createElement('button')
+    let del     = document.createElement('button')
+
+    up.innerHTML    = '<i class="fa-solid fa-angle-up"></i>'
+    down.innerHTML  = '<i class="fa-solid fa-angle-down"></i>'
+    del.innerHTML   = '<i class="fa-solid fa-xmark"></i>'
+
+    up.onclick = function ()    { changePlace('up', key) }
+    down.onclick = function ()  { changePlace('down', key) }
+    del.onclick = function ()   { changePlace('del', key) }
+
+    let cmd     = document.createElement('div')
+    cmd.classList.add('cmd')
+    cmd.append(up, down, del)
+
+    let div = document.createElement('div')
+    div.classList.add('item')
+
+    div.appendChild(document.createElement('p')).appendChild(document.createTextNode(e.title))
+    div.append(img, cmd)
+    div.setAttribute('id', 'item-' + e.id)
+    // div.setAttribute('draggable', true)
+    listItemEdit.appendChild(div)
 
 
-function showEditList(listEdit) {
-    listEdit.innerHTML = ""
-    let list = JSON.parse(localStorage.getItem('saveList'))
-    
-    if (list!=null) {
-        list.forEach(e => {
-            let img = document.createElement('img')
-            img.src = e.song_art_image_thumbnail_url
-            img.setAttribute("width",   35);
-        img.setAttribute("height",  35);
-
-        let up      = document.createElement('button')
-        let down    = document.createElement('button') 
-        let del     = document.createElement('button') 
-        
-        up.innerHTML    = '<i class="fa-solid fa-angle-up"></i>'
-        down.innerHTML  = '<i class="fa-solid fa-angle-down"></i>'
-        del.innerHTML   = '<i class="fa-solid fa-xmark"></i>'
-        
-        up.onclick = function ()    { changePlace('up', e.id) }
-        down.onclick = function ()  { changePlace('down', e.id) }
-        del.onclick = function ()   { changePlace('del', e.id) }
-        
-        let cmd     = document.createElement('div')
-        cmd.classList.add('cmd')
-        cmd.append(up, down, del)
-        
-        let div = document.createElement('div')
-        div.classList.add('item')
-        
-        div.appendChild(document.createElement('p')).appendChild(document.createTextNode(e.title))
-        div.append(img, cmd)
-        div.setAttribute('id', 'item-' + e.id)
-        listEdit.appendChild(div)
-
-        
-        })
-    }
+    })
 }
 
-function changePlace(cmd, id) {
+
+function changePlace(cmd, index) {
 
     let list = JSON.parse(localStorage.getItem('saveList'))
 
-    // let item = document.getElementById("item-" + id)
+    console.log(list);
 
-    let listDom = document.querySelector('.list-item-edit')
+    let line = list[index]
 
-    let arr = []
+    list.splice(index, 1)
+    list.splice(index, 0, line)
 
-    listDom.childNodes.forEach(e => {
-        arr.push(e)
-    });
+    // list.splice(index-1, 1, line);
 
+    // localStorage.setItem('saveList', JSON.stringify(list))
 
-    // ICI ************************************************************************
+    console.log(list);
 
-    
-    console.log(arr);
-
-    // console.log(item.parentElement);
+    // let tab = document.querySelector(".list-item-edit")
 
 
+    // if (cmd == 'up') {
+    //     let move = tab.children[index]
+    //     // tab.removeChild(tab.childNodes[index])
+    //     tab.insertBefore(move, tab.children[index-1])
+    // }
+    // if (cmd == 'down') {
+    //     let move = tab.children[index]
+    //     tab.removeChild(tab.childNodes[index])
+    //     tab.insertBefore(move, tab.children[index+1])
+    // }
 
-    if (cmd == 'del') {
-        document.getElementById("item-" + id).remove();
-    }
+    // if (cmd == 'del') {
+    //     // tab.removeChild(tab.childNodes[index])
+    // }
 
 
 
@@ -87,18 +93,18 @@ function changePlace(cmd, id) {
     // localStorage.setItem('saveList', JSON.stringify(list))
 
     // listItemEdit = showEditList(listItemEdit)
-    
+
     // console.log(list);
 
     // list.forEach(e => {
-        
+
     // });
 
     // switch (cmd) {
     //     case 'up':
-            
+
     //         break;
-    
+
     //     default:
     //         break;
     // }
